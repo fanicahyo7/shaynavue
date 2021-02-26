23afa471bbfcc2ef860e29e69f0dadb7be90d41c
fanicahyo7 <fanidc7@gmail.com>
2021-02-26 20:41:46 +0700

    home fix

--- src/components/WomenShayna.vue
+++ src/components/WomenShayna.vue
@@ -5,7 +5,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-12 mt-5">
-                    <carousel class="product-slider" :items="3" :nav="false" :autoplay="true">
+                    <carousel class="product-slider" :items="3" :nav="false" :autoplay="true" :dots="false">
                         <div class="product-item">
                             <div class="pi-pic">
                                 <img src="img/mickey1.jpg" alt="" />
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,9 @@
    <HeaderShayna />
    <HeroShayna />
    <WomenShayna />
+   <InstaShayna />
+   <PartnerShayna />
+   <FooterShayna />
   </div>
 </template>
 
@@ -12,6 +15,9 @@
 import HeaderShayna from '@/components/HeaderShayna.vue'
 import HeroShayna from '@/components/HeroShayna.vue'
 import WomenShayna from '@/components/WomenShayna.vue'
+import InstaShayna from '@/components/InstaShayna.vue'
+import PartnerShayna from '@/components/PartnerShayna.vue'
+import FooterShayna from '@/components/FooterShayna.vue'
 
 export default {
   name: 'Home',
@@ -19,7 +25,10 @@ export default {
     // HelloWorld
     HeaderShayna,
     HeroShayna,
-    WomenShayna
+    WomenShayna,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna
   }
 }
 </script>
